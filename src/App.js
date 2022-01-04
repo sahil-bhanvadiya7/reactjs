db2c8ed780ab099ab4fb2ef84a837771562608bb
Sahil Bhanvadiya <180120107008@git.org.in>
2022-01-04 18:43:15 +0530

    till 4 jan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import NewExpense from "./compoent/NewExpense/NewExpense";
+import Expenses from "./compoent/Expenses/Expenses";
 function App() {
+  const expenses = [
+    {
+      id: "a1",
+      title: "hello",
+      amount: 190,
+      date: new Date(2022, 0, 1),
+    },
+    {
+      id: "a2",
+      title: "hello2",
+      amount: 200,
+      date: new Date(2022, 1, 22),
+    },
+    {
+      id: "a3",
+      title: "hello3",
+      amount: 100,
+      date: new Date(2022, 8, 12),
+    },
+    {
+      id: "a4",
+      title: "hello4",
+      amount: 90,
+      date: new Date(2022, 5, 22),
+    },
+  ];
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses item={expenses}/>
+     
     </div>
   );
 }
