374aa40e29d30241a58461a480c4e5519fdd16e7
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-05 14:10:07 +0530

    change

--- src/compoent/NewExpense/ExpenseForm.js
+++ src/compoent/NewExpense/ExpenseForm.js
@@ -59,7 +59,7 @@ function ExpenseForm(props) {
           <label>Date</label>
           <input
             type="date"
-            min="2022-01-01"
+            min="2019-01-01"
             max="2022-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
