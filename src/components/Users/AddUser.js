f26b727acb2c650c1f1cbaffec2e53d9318a3816
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-05 19:10:15 +0530

    91 completed

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
 function AddUser(props) {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
 
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    }
-
-    return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type='text'/>
-            <label htmlFor='age'>Age (years)</label>
-            <input id='age' type='number'/>
-            <button type='submit'>Add User</button>
-        </form>
-    )
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 }
 
-export default AddUser
+export default AddUser;
