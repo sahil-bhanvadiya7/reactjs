201dae3f7f370a1d170ab9cfaaf0f7403af9bfb6
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-06 16:11:20 +0530

    125 completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { AuthContextProvider } from './components/store/auth-context';
+ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
+  document.getElementById("root")
+);
