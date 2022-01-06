6227fe23b52282d0a6fc96a89fc291b78d1ff64b
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-06 11:08:25 +0530

    105 completed

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,25 +1,46 @@
-import React from 'react'
-import Button from './Button';
-import Card from './Card';
+import React from "react";
+import Button from "./Button";
+import Card from "./Card";
+import classes from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
 
-import classes from './ErrorModal.module.css';
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.action}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 function ErrorModel(props) {
-    return (
-        <div>
-        <div className={classes.backdrop} onClick={props.onConfirm}/>
-        <Card className={classes.modal}>
-            <header className={classes.header}>
-                <h2>{props.title}</h2>
-            </header>
-            <div className={classes.content}>
-                <p>{props.message}</p>
-            </div>
-            <footer className={classes.action}>
-                <Button onClick={props.onConfirm}>Okay</Button>
-            </footer>
-        </Card>
-        </div>
-    )
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
+  );
 }
 
-export default ErrorModel
+export default ErrorModel;
