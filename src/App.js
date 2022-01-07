af4c6841048ef714c3da02c250d73c723f258c76
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-07 18:18:36 +0530

    157 completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { useState , useCallback} from 'react';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+
+import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
-import Button from './components/UI/Button/Button'
+import './App.css';
 
 function App() {
-  console.log("App Running");
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log('APP RUNNING');
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph)
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
-      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
