d5a313ab27e5f0976ca79749ef2561d76333bd34
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-10 11:28:26 +0530

    169 completed

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,26 +1,27 @@
 // import { render } from "@testing-library/react";
 import { Component } from "react";
+import UsersContext from "../store/users-context";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
-
 class UserFinder extends Component {
+  static contextType = UsersContext;
   constructor() {
-      super();
+    super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
   }
+
+componentDidMount(){
+    this.setState({ filteredUsers: this.context.users})
+}
+
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
