6464fcf18d983b1aeeb16c25ce64030b49e9a81d
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-17 11:45:49 +0530

    199 completed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import SimpleInput from './components/SimpleInput';
+import React from 'react';
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default App;
