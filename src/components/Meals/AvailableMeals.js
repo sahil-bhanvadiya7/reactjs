2bb3f58a1e656fec000064d212b31e5a8d0b2a16
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-17 17:15:04 +0530

    217 completed

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,35 +4,9 @@ import React from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-// const DUMMY_MEALS = [
-//   {
-//     id: "m1",
-//     name: "Sushi",
-//     description: "Finest fish and veggies",
-//     price: 22.99,
-//   },
-//   {
-//     id: "m2",
-//     name: "Schnitzel",
-//     description: "A german specialty!",
-//     price: 16.5,
-//   },
-//   {
-//     id: "m3",
-//     name: "Barbecue Burger",
-//     description: "American, raw, meaty",
-//     price: 12.99,
-//   },
-//   {
-//     id: "m4",
-//     name: "Green Bowl",
-//     description: "Healthy...and green...",
-//     price: 18.99,
-//   },
-// ];
-
 function AvailableMeals() {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
@@ -49,9 +23,15 @@ function AvailableMeals() {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
+
+if(isLoading){
+  return <section className={classes.MealsLoading}><p>Loading...</p></section>
+}
+
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
