4337a874eeb96ecff8e90798fc79047269447e1e
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-17 19:18:49 +0530

    223 completed

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -17,18 +17,16 @@ const cartReducer = (state, action) => {
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
-        if(existingCartItem){
-           const updatedItem ={
-                ...existingCartItem,
-                amount : existingCartItem.amount + action.item.amount
-            }
-            updatedItems = [...state.items];
-            updatedItems[existingCartItemIndex] = updatedItem;
-        } else{
-            updatedItems = state.items.concat(action.item)
-        }
-
-
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
 
     return {
       items: updatedItems,
@@ -36,27 +34,28 @@ const cartReducer = (state, action) => {
     };
   }
 
-  if(action.type === "REMOVE"){
-      
-      const existingCartItemIndex = state.items.findIndex(
-          (item) => item.id === action.id
-          );
-          const existingItem = state.items[existingCartItemIndex]
-          const updatedTotalAmount = state.totalAmount - existingItem.price;
-          let updatedItems;
-          if(existingItem.amount === 1){
-              updatedItems = state.items.filter(item => item.id !== action.id)
-          }else{
-              const updatedItem = {...existingItem, amount: existingItem.amount -1};
-              updatedItems = [...state.items]
-              updatedItems[existingCartItemIndex] = updatedItem;
-          }
-          return{
-              items: updatedItems,
-              totalAmount: updatedTotalAmount
-          }
-}
-
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
   return defaultCartState;
 };
 
@@ -72,12 +71,15 @@ function CartProvider(props) {
   const removeItemFromCartHadler = (id) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
-
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHadler,
     removeItem: removeItemFromCartHadler,
+    clearCart: clearCartHandler,
   };
 
   return (
