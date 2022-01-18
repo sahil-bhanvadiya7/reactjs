7a91ce497135ca44dcb19677052c32366a69ddb8
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-18 19:08:16 +0530

    262 completed

--- src/App.js
+++ src/App.js
@@ -12,15 +12,17 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-useEffect(() => {
-  dispatch(fetchCartData())
-}, [dispatch])
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
   useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
-    dispatch(sendCartData(cart));
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -13,16 +13,21 @@ export const fetchCartData = () => {
       return data;
     };
     try {
-       const cartData = await fetchData();
-       dispatch(cartActions.replaceCart(cartData));
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
-        dispatch(
-            uiActions.showNotification({
-              status: "error",
-              title: "Error!",
-              message: "Sending cart data failed!",
-            })
-          );
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
     }
   };
 };
@@ -42,7 +47,10 @@ export const sendCartData = (cart) => {
         "https://react-http-821d9-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
