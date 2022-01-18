913d3c013460eebd6f2e45daed1c67b13cba1be3
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-18 17:54:32 +0530

    257 completed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector(state=>state.cart)
+  useEffect(() => {
+    fetch('https://react-http-821d9-default-rtdb.firebaseio.com/cart.json',{
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart])
   return (
     <Layout>
       {showCart && <Cart />}
