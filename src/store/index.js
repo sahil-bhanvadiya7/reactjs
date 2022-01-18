e78c02eaa212b109244aca21b71d10d02f53dcad
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-18 13:57:01 +0530

    244 completed

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 const initialState = { counter: 0, showCounter: true };
 const counterSlice = createSlice({
@@ -12,7 +11,7 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
@@ -21,7 +20,7 @@ const counterSlice = createSlice({
 });
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: counterSlice.reducer,
 });
-
+export const counterActions = counterSlice.actions;
 export default store;
