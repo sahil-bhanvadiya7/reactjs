225e20d5f0d3a7fcd53909a06b787dafc67b992d
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-18 14:36:19 +0530

    245 completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Counter from './components/Counter';
-
-
+import Auth from './components/Auth'
+import Header from './components/Header'
+import UserProfile from './components/UserProfile'
 function App() {
-  return (
+  return (<>
+    <Header/>
+    <Auth/>
     <Counter />
+    </>
   );
 }
 
