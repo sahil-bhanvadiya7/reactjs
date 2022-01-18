f0612356d59526890d0aef57019e60ddf94072b5
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-18 18:58:20 +0530

    261 completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { sendCartData } from "./store/cart-slice";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -12,7 +12,9 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-
+useEffect(() => {
+  dispatch(fetchCartData())
+}, [dispatch])
   useEffect(() => {
     if (isInitial) {
       isInitial = false;
