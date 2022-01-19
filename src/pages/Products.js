d187c562f02e68053c1726bb8672dc6ca2a5e865
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-19 14:43:00 +0530

    274 completed

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Products() {
   return (
     <section>
       <h1>Products are here..</h1>
-      <li>A BOOK</li>
-      <li>A carpet</li>
-      <li>An online Course</li>
+      <li><Link to='/products/p1'> A book </Link></li>
+      <li><Link to='/products/p2'> A carpet </Link></li>
+      <li><Link to='/products/p3'> An online course </Link></li>
+    
     </section>
   );
 }
