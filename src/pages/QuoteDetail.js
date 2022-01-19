3b673c1a03fc7a2f4d46a966adbe6881dfa90e9a
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-19 16:34:28 +0530

    282 completed

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,33 @@
 import React from "react";
 import Comments from "../components/comments/Comments";
 import { useParams, Route } from "react-router-dom";
+import HighLightedQuote from '../components/quotes/HighlightedQuote'
+const DUMMY_QUOTES = [
+    {
+      id: "q1",
+      author: "xyz",
+      text: "Learning React is fun!",
+    },
+    {
+      id: "q2",
+      author: "abc",
+      text: "Learning React is fun!",
+    },
+    {
+      id: "q3",
+      author: "spd",
+      text: "Learning React is fun!",
+    },
+  ];
 function QuoteDetail() {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find(quote=> quote.id === params.quoteId);
+  if(!quote){
+      return<p>NO Quote found!!!</p>
+  }
   return (
     <>
-      <div> quote detail page</div>
-      <h1>{params.quoteId}</h1>
+      <HighLightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
