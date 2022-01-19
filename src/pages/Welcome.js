0864e5184cdf98b474df390684d6fc775c4b1c26
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-19 14:47:06 +0530

    275 completed

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,14 @@
-import React from 'react';
-
+import React from "react";
+import { Route } from "react-router-dom";
 function Welcome() {
-  return <div>Welcome.</div>;
+  return (
+    <>
+      <div>Welcome.</div>
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user</p>
+      </Route>
+    </>
+  );
 }
 
 export default Welcome;
