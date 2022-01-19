e418d3c3226b43644e7dc411909b9e46bc901693
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-19 16:46:01 +0530

    283 completed

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="centered">
+      <p style={{ textAlign: "center" }}>page not found</p>
+    </div>
+  );
+}
+
+export default NotFound;
