459ec27c64403a972b4eb9269d544f0e8fb5a149
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-19 16:55:33 +0530

    284 completed

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 function NewQuote() {
+    const history = useHistory();
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData)
+        history.push('/quotes')
     }
     return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
