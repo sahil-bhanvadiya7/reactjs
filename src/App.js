dd30aa98ac52ca7cca8097b40d2f72923de4e1f4
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-19 14:13:34 +0530

    270 completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import Products from "./components/Products";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./component/MainHeader";
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
