b1c224e4e6a83ba50dc0aaeb93a59dac10786bde
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-01-20 11:24:54 +0530

    example-1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route
+            path="/products/:productId"
+            element={<ProductDetail />}
+          ></Route>
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
